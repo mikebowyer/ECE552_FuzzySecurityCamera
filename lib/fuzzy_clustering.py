32b5965db36db8e41bba6f23b6e8225b9d24c48e
Michael Bowyer <mbowyer@umich.edu>
2020-12-10 20:17:29 -0500

    refactored further

--- lib/fuzzy_clustering.py
+++ lib/fuzzy_clustering.py
@@ -12,6 +12,33 @@
 ## License: {license}
 ## Status: {dev_status}
 ##################################################
+import numpy as np
+import skfuzzy as fuzz
 
-def fuzzyClusteringOnImage(inputImage):
-    print("Test")
+
+def fuzzyClusteringOnImage(inputImage, camRes):
+    colors = ['black', 'orange', 'grey']
+
+    # Define three cluster centers and parameters
+    centers = [[0, 0, 0],
+               [122, 122, 122],
+               [255, 255, 255]]
+    numClusters = 3
+    maxIter = 100
+
+    redPixelValues = inputImage[:, :, 0].flatten()
+    greenPixelValues = inputImage[:, :, 1].flatten()
+    bluePixelValues = inputImage[:, :, 2].flatten()
+    mydata = np.vstack((redPixelValues, greenPixelValues, bluePixelValues))
+    cntr, u, u0, d, jm, p, fpc = fuzz.cluster.cmeans(
+        mydata, c=numClusters, m=2, error=0.005, maxiter=1000)
+
+    numPixels = len(u[0, :])
+    pixelClassification = np.empty(numPixels)
+    for j in range(0, numPixels):
+        pixelClassification[j] = u[:, j].argmax()
+
+    pixelClassifiedImage = pixelClassification.reshape(
+        camRes.height, camRes.width)
+
+    return pixelClassifiedImage
--- security_cam_start.py
+++ security_cam_start.py
@@ -1,14 +1,14 @@
 
 # External Library Imports
 from picamera import PiCamera
+import numpy as np
 # Internal Library Imports
 from lib import fuzzy_clustering as fcm
 
 from time import sleep
-import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
-import skfuzzy as fuzz
+
 
 # Camera Parametesr and Initialization
 print("Intialization Camera")
@@ -28,31 +28,8 @@ for i in range(5):
     print("Saving Original Image")
     im.save('./outputImages/%s_OriginalImage.jpg' % i)
 
-    fcm.fuzzyClusteringOnImage(output)
-
-    colors = ['black', 'orange', 'grey']
-
-    # Define three cluster centers and parameters
-    centers = [[0, 0, 0],
-               [122, 122, 122],
-               [255, 255, 255]]
-    numClusters = 3
-    maxIter = 100
-
-    redPixelValues = output[:, :, 0].flatten()
-    greenPixelValues = output[:, :, 1].flatten()
-    bluePixelValues = output[:, :, 2].flatten()
-    mydata = np.vstack((redPixelValues, greenPixelValues, bluePixelValues))
-    cntr, u, u0, d, jm, p, fpc = fuzz.cluster.cmeans(
-        mydata, c=numClusters, m=2, error=0.005, maxiter=1000)
-
-    numPixels = len(u[0, :])
-    pixelClassification = np.empty(numPixels)
-    for j in range(0, numPixels):
-        pixelClassification[j] = u[:, j].argmax()
-
-    pixelClassifiedImage = pixelClassification.reshape(img_height, img_width)
+    clusteredImg = fcm.fuzzyClusteringOnImage(output, camera.resolution)
 
     print("Saving Segmented Image")
     imgName = './outputImages/%s_ClusteredImage.jpg' % i
-    plt.imsave(imgName, pixelClassifiedImage)
+    plt.imsave(imgName, clusteredImg)
