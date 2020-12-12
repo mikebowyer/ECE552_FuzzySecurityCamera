e6a60d21a409a196dc0da39292af09a91b601268
Michael Bowyer <mbowyer@umich.edu>
2020-12-12 13:34:40 -0500

    Added logging

--- security_cam_start.py
+++ security_cam_start.py
@@ -1,14 +1,18 @@
-
 # External Library Imports
-from picamera import PiCamera
 import cv2
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+import logging
 # Internal Library Imports
 from lib import fuzzy_clustering as fcm
 from lib import centroid_calculator as cc
+
+logging.basicConfig(
+    format='[%(levelname)s\t%(asctime)s] %(message)s', datefmt='%I:%M:%S')
+logging.getLogger().setLevel(logging.DEBUG)
+logging.getLogger('matplotlib.font_manager').disabled = True
 
 
 def capturePic(cam):
@@ -19,8 +23,20 @@ def capturePic(cam):
     return ret, image
 
 
+def saveClusteredImg(img, clusterIds, fileName):
+    # Establish color map
+    colorMap = fcm.createColorMap(brightestClustIDs)
+    # Create image to save
+    fig, ax = plt.subplots(1)
+    ax.imshow(clusteredImg, cmap=colorMap)
+    circle = patches.Circle((x, y), 4, linewidth=1,
+                            edgecolor='r', facecolor='r')
+    ax.add_patch(circle)
+    # save image
+    plt.savefig(imgName)
+
     # Camera Parametesr and Initialization
-print("Intialization Camera")
+logging.info("Initializing Camera")
 cam = cv2.VideoCapture(1)
 img_width = 320
 img_height = 240
@@ -28,27 +44,20 @@ cam.set(3, img_width)
 cam.set(4, img_height)
 
 for i in range(5):
-    print("Taking Image Number: %s" % i)
+    logging.info("Capturing Image Number - %s" % i)
     ret, imgArray = capturePic(cam)
 
-    print("Saving Original Image")
+    logging.debug("Saving Original Image")
     decodedImg = Image.fromarray(imgArray)
     decodedImg.save('./outputImages/%s_OriginalImage.jpg' % i)
 
-    print("Performing fuzzy c means")
+    logging.debug("Starting fuzzy c means clustering")
     clusteredImg, brightestClustIDs = fcm.fuzzyClusteringOnImage(
         imgArray, [img_width, img_height])
-
-    colorMap = fcm.createColorMap(brightestClustIDs)
 
     x, y = cc.findBrightClusterCenter(
         clusteredImg, brightestClustIDs[0][0])
 
-    print("Saving Segmented Image")
-    fig, ax = plt.subplots(1)
-    ax.imshow(clusteredImg, cmap=colorMap)
-    circle = patches.Circle((x, y), 4, linewidth=1,
-                            edgecolor='r', facecolor='r')
-    ax.add_patch(circle)
+    logging.info("Saving Segmented Image")
     imgName = './outputImages/%s_ClusteredImage.jpg' % i
-    plt.savefig(imgName)
+    saveClusteredImg(clusteredImg, brightestClustIDs, imgName)
