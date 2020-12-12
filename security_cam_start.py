1dba9ea750c95766c414adf0e3d9ed2be180bf21
Michael Bowyer <mbowyer@umich.edu>
2020-12-12 12:23:27 -0500

    working resolution change and capture with webcam

--- security_cam_start.py
+++ security_cam_start.py
@@ -1,6 +1,7 @@
 
 # External Library Imports
 from picamera import PiCamera
+import cv2
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
@@ -10,37 +11,50 @@ from lib import fuzzy_clustering as fcm
 from lib import centroid_calculator as cc
 
 
+def capturePic(cam):
+    ret = 0
+    image = 0
+    for i in range(0, 10):
+        ret, image = cam.read()
+    return ret, image
+
+
 # Camera Parametesr and Initialization
-print("Intialization Camera")
-camera = PiCamera()
-camera.rotation = 0
+cam = cv2.VideoCapture(1)
 img_width = 320
 img_height = 240
-camera.resolution = (img_width, img_height)
+cam.set(3, img_width)
+cam.set(4, img_height)
+
+# print("Intialization Camera")
+# camera = PiCamera()
+# camera.rotation = 0
+# camera.resolution = (img_width, img_height)
 
 for i in range(5):
     print("Taking Image Number: %s" % i)
-    # camera.capture('/home/pi/Desktop/image%s.jpg' % i)
-    output = np.empty((240, 320, 3), dtype=np.uint8)
-    camera.capture(output, 'rgb')
-    im = Image.fromarray(output)
+    ret, img = capturePic(cam)
+    imgToSave = Image.fromarray(img)
     print("Saving Original Image")
-    im.save('./outputImages/%s_OriginalImage.jpg' % i)
+    imgToSave.save('./outputImages/%s_OriginalImage.jpg' % i)
 
-    clusteredImg, brightestClustIDs = fcm.fuzzyClusteringOnImage(
-        output, camera.resolution)
+    # output = np.empty((240, 320, 3), dtype=np.uint8)
+    # camera.capture(output, 'rgb')
 
-    x, y = cc.findBrightClusterCenter(
-        clusteredImg, brightestClustIDs[0][0])
+    # clusteredImg, brightestClustIDs = fcm.fuzzyClusteringOnImage(
+    #     output, camera.resolution)
 
-    print("Saving Segmented Image")
-    fig, ax = plt.subplots(1)
-    ax.imshow(clusteredImg)
-    circle = patches.Circle((x, y), 4, linewidth=1,
-                            edgecolor='r', facecolor='none')
-    ax.add_patch(circle)
-    imgName = './outputImages/%s_ClusteredImage.jpg' % i
-    plt.savefig(imgName)
+    # x, y = cc.findBrightClusterCenter(
+    #     clusteredImg, brightestClustIDs[0][0])
+
+    # print("Saving Segmented Image")
+    # fig, ax = plt.subplots(1)
+    # ax.imshow(clusteredImg)
+    # circle = patches.Circle((x, y), 4, linewidth=1,
+    #                         edgecolor='r', facecolor='none')
+    # ax.add_patch(circle)
+    # imgName = './outputImages/%s_ClusteredImage.jpg' % i
+    # plt.savefig(imgName)
 
     # plt.imshow(clusteredImg)
     # plt.scatter(x, y, s=500, c='red', marker='o')
