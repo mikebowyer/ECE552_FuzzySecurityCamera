916c0aaa9c029e361425ae00108e5d797012920f
Michael Bowyer <mbowyer@umich.edu>
2020-12-13 12:32:23 -0500

    Updated servo control for independent controlling

--- servo_control.py
+++ servo_control.py
@@ -3,19 +3,17 @@ from time import sleep
 
 
 def getDutyFromAngle(angle):
-    duty = (angle/18) + 7
+    duty = (-angle/18) + 7
     return duty
 
 
 def controlServo(servo, gpiopin, duty):
-    # print(duty)
     GPIO.output(11, True)
     GPIO.output(13, True)
     servo.ChangeDutyCycle(duty)
-    sleep(1)
     GPIO.output(11, False)
     GPIO.output(13, False)
-    # servo.ChangeDutyCycle(duty)
+    servo.ChangeDutyCycle(0)
 
 
 GPIO.cleanup()
@@ -23,35 +21,48 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.OUT)
 GPIO.setup(13, GPIO.OUT)
 
-leftServo = GPIO.PWM(11, 50)
-rightServo = GPIO.PWM(13, 50)
-leftServo.start(0)
-rightServo.start(0)
+vertServo = GPIO.PWM(11, 50)
+horizServo = GPIO.PWM(13, 50)
+horizServo.start(0)
+vertServo.start(0)
 
-leftAngle = 0
-rightAngle = 0
+horizontalAngle = 0
+verticalAngle = 0
+controlling = 'horizontal'
 
-controlServo(leftServo, 11, getDutyFromAngle(leftAngle))
-controlServo(rightServo, 13, getDutyFromAngle(rightAngle))
+controlServo(horizServo, 11, getDutyFromAngle(horizontalAngle))
+controlServo(vertServo, 13, getDutyFromAngle(verticalAngle))
 
 while(1):
     usrinput = input("Angle you want servos at (p = +5 deg, l = -5 deg): ")
     # print(f'You entered {usrinput}')
-    if(usrinput == "p"):
-        leftAngle += 15
-        rightAngle += 15
-    elif(usrinput == "l"):
-        leftAngle += -15
-        rightAngle += -15
+    if(usrinput == "v"):
+        controlling = "vertical"
+    elif(usrinput == "h"):
+        controlling = "horizontal"
     else:
-        leftAngle = int(usrinput)
-        rightAngle = int(usrinput)
+        if(usrinput == "p"):
+            if(controlling == "horizontal"):
+                horizontalAngle += 15
+            else:
+                verticalAngle += 15
+        elif(usrinput == "l"):
+            if(controlling == "horizontal"):
+                horizontalAngle += -15
+            else:
+                verticalAngle += -15
+        else:
+            if(controlling == "horizontal"):
+                horizontalAngle = int(usrinput)
+            else:
+                verticalAngle = int(usrinput)
 
-    print("Current Servo Angle: {}".format(leftAngle))
+    print("Vertical Servo Angle: {}".format(verticalAngle))
+    print("Horizontal Servo Angle: {}".format(horizontalAngle))
 
-    controlServo(leftServo, 11, getDutyFromAngle(leftAngle))
-    controlServo(rightServo, 13, getDutyFromAngle(rightAngle))
+    controlServo(horizServo, 11, getDutyFromAngle(horizontalAngle))
+    controlServo(vertServo, 13, getDutyFromAngle(verticalAngle))
 
-leftServo.stop()
-rightServo.stop()
+horizServo.stop()
+vertServo.stop()
 GPIO.cleanup()
