5494d4807180d8cb449a75d917e5f4ed79fc4165
Michael Bowyer <mbowyer@umich.edu>
2020-12-13 15:01:03 -0500

    Got defuzzification working for change in angle

--- lib/fuzzy_servo_controller.py
+++ lib/fuzzy_servo_controller.py
@@ -25,7 +25,7 @@ class fuzzyServoSetPointChangeCalc:
         self.createhorizontalMembershipFunctions()
 
         self.calcInputMemFuncActivations(-110)
-        self.applyRules()
+        self.applyRulesAndDefuzzify()
 
     def createhorizontalMembershipFunctions(self):
         # Generate input and output analog variable ranges
@@ -109,29 +109,29 @@ class fuzzyServoSetPointChangeCalc:
         self.act_pixFromCenterErrHorz_Right = fuzz.interp_membership(
             self.range_pixFromCenterErrHorz, self.mf_pixFromCenterErrHorz_Right, pixFromCenterErrHorz)
         self.act_pixFromCenterErrHorz_VeryRight = fuzz.interp_membership(
-            self.range_pixFromCenterErrHorz, self.mf_pixFromCenterErrHorz_Left, pixFromCenterErrHorz)
+            self.range_pixFromCenterErrHorz, self.mf_pixFromCenterErrHorz_VeryRight, pixFromCenterErrHorz)
 
-    def applyRules(self):
+    def applyRulesAndDefuzzify(self):
         # Rule 1 - If Very Left input, very left output
         act_rule1 = self.act_pixFromCenterErrHorz_VeryLeft
         act_servoAngleChangeHorz_VeryLeft = np.fmin(
-            act_rule1, self.mf_servoAngleChangeHorz_VeryLeft)  # removed entirely to 0
+            act_rule1, self.mf_servoAngleChangeHorz_VeryLeft)
         # Rule 2 - If Left input, left output
         act_rule2 = self.act_pixFromCenterErrHorz_Left
         act_servoAngleChangeHorz_Left = np.fmin(
-            act_rule2, self.mf_servoAngleChangeHorz_Left)  # removed entirely to 0
+            act_rule2, self.mf_servoAngleChangeHorz_Left)
         # Rule 3 - If Center input, DontMove output
         act_rule3 = self.act_pixFromCenterErrHorz_Center
         act_servoAngleChangeHorz_DontMove = np.fmin(
-            act_rule3, self.mf_servoAngleChangeHorz_DontMove)  # removed entirely to 0
+            act_rule3, self.mf_servoAngleChangeHorz_DontMove)
         # Rule 4 - If right input, right output
         act_rule4 = self.act_pixFromCenterErrHorz_Right
         act_servoAngleChangeHorz_Right = np.fmin(
-            act_rule4, self.mf_servoAngleChangeHorz_Right)  # removed entirely to 0
+            act_rule4, self.mf_servoAngleChangeHorz_Right)
         # Rule 5 - If Very right input, very right output
         act_rule5 = self.act_pixFromCenterErrHorz_VeryRight
         act_servoAngleChangeHorz_VeryRight = np.fmin(
-            act_rule5, self.mf_servoAngleChangeHorz_VeryRight)  # removed entirely to 0
+            act_rule5, self.mf_servoAngleChangeHorz_VeryRight)
 
         act_zeros = np.zeros_like(self.range_servoAngleChangeHorz)
 
@@ -139,17 +139,77 @@ class fuzzyServoSetPointChangeCalc:
             # Visualize this
             fig, ax0 = plt.subplots()
 
+            # Plot Very Left Output Membership Values
             ax0.fill_between(self.range_servoAngleChangeHorz, act_zeros, act_servoAngleChangeHorz_VeryLeft,
                              facecolor='b', alpha=0.7)
             ax0.plot(self.range_servoAngleChangeHorz, self.mf_servoAngleChangeHorz_VeryLeft,
                      'b', linewidth=0.5, linestyle='--', )
-            # ax0.fill_between(x_tip, tip0, tip_activation_md,
-            #                  facecolor='g', alpha=0.7)
-            # ax0.plot(x_tip, tip_md, 'g', linewidth=0.5, linestyle='--')
-            # ax0.fill_between(x_tip, tip0, tip_activation_hi,
-            #                  facecolor='r', alpha=0.7)
-            # ax0.plot(x_tip, tip_hi, 'r', linewidth=0.5, linestyle='--')
-            # ax0.set_title('Output membership activity')
+
+            # Plot Left Output Membership Values
+            ax0.fill_between(self.range_servoAngleChangeHorz, act_zeros, act_servoAngleChangeHorz_Left,
+                             facecolor='c', alpha=0.7)
+            ax0.plot(self.range_servoAngleChangeHorz, self.mf_servoAngleChangeHorz_Left,
+                     'c', linewidth=0.5, linestyle='--', )
+
+            # Plot Dont Move Output Membership Values
+            ax0.fill_between(self.range_servoAngleChangeHorz, act_zeros, act_servoAngleChangeHorz_DontMove,
+                             facecolor='g', alpha=0.7)
+            ax0.plot(self.range_servoAngleChangeHorz, self.mf_servoAngleChangeHorz_DontMove,
+                     'g', linewidth=0.5, linestyle='--', )
+
+            # Plot Right Output Membership Values
+            ax0.fill_between(self.range_servoAngleChangeHorz, act_zeros, act_servoAngleChangeHorz_Right,
+                             facecolor='y', alpha=0.7)
+            ax0.plot(self.range_servoAngleChangeHorz, self.mf_servoAngleChangeHorz_Right,
+                     'y', linewidth=0.5, linestyle='--', )
+
+            # Plot Very Right Output Membership Values
+            ax0.fill_between(self.range_servoAngleChangeHorz, act_zeros, act_servoAngleChangeHorz_VeryRight,
+                             facecolor='r', alpha=0.7)
+            ax0.plot(self.range_servoAngleChangeHorz, self.mf_servoAngleChangeHorz_VeryRight,
+                     'r', linewidth=0.5, linestyle='--', )
+            ax0.set_title(
+                'Membership values of output membership functions after rule application')
+            # Turn off top/right axes
+            for ax in (ax0,):
+                ax.spines['top'].set_visible(False)
+                ax.spines['right'].set_visible(False)
+                ax.get_xaxis().tick_bottom()
+                ax.get_yaxis().tick_left()
+
+            plt.tight_layout()
+            plt.draw()
+            plt.show()
+
+        # Defuzzify
+        aggregated = np.fmax(act_servoAngleChangeHorz_VeryLeft, np.fmax(act_servoAngleChangeHorz_Left, np.fmax(
+            act_servoAngleChangeHorz_DontMove, np.fmax(act_servoAngleChangeHorz_Right, act_servoAngleChangeHorz_VeryRight))))
+
+        output_servoAngleChangeHorz = fuzz.defuzz(self.range_servoAngleChangeHorz,
+                                                  aggregated, 'centroid')
+
+        if(self.plotOrNot):
+            output_activation = fuzz.interp_membership(
+                self.range_servoAngleChangeHorz, aggregated, output_servoAngleChangeHorz)  # for plot
+
+            # Visualize this
+            fig, ax0 = plt.subplots()
+
+            ax0.plot(self.range_servoAngleChangeHorz, self.mf_servoAngleChangeHorz_VeryLeft,
+                     'b', linewidth=0.5, linestyle='--', )
+            ax0.plot(self.range_servoAngleChangeHorz, self.mf_servoAngleChangeHorz_Left,
+                     'c', linewidth=0.5, linestyle='--')
+            ax0.plot(self.range_servoAngleChangeHorz, self.mf_servoAngleChangeHorz_DontMove,
+                     'g', linewidth=0.5, linestyle='--')
+            ax0.plot(self.range_servoAngleChangeHorz, self.mf_servoAngleChangeHorz_Right,
+                     'y', linewidth=0.5, linestyle='--')
+            ax0.plot(self.range_servoAngleChangeHorz, self.mf_servoAngleChangeHorz_VeryRight,
+                     'r', linewidth=0.5, linestyle='--')
+            ax0.fill_between(self.range_servoAngleChangeHorz, act_zeros, aggregated,
+                             facecolor='Orange', alpha=0.7)
+            ax0.plot([output_servoAngleChangeHorz, output_servoAngleChangeHorz], [0, output_activation],
+                     'k', linewidth=1.5, alpha=0.9)
+            ax0.set_title('Aggregated membership and result (line)')
 
             # Turn off top/right axes
             for ax in (ax0,):
