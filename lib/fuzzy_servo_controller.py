a3dac9b894952c64b4418da21a695d25616fd3b7
Michael Bowyer <mbowyer@umich.edu>
2020-12-13 14:19:36 -0500

    Got calculation of activation functions working

--- lib/fuzzy_servo_controller.py
+++ lib/fuzzy_servo_controller.py
@@ -22,66 +22,66 @@ class fuzzyServoSetPointChangeCalc:
     def __init__(self):
 
         plotOrNot = True
-        self.createMembershipFunctions(plotOrNot)
+        self.createhorizontalMembershipFunctions(plotOrNot)
 
-    def createMembershipFunctions(self, plotOrNot):
-        # Generate universe variables
-        #   * Quality and service on subjective ranges [0, 10]
-        #   * Tip has a range of [0, 25] in units of percentage points
-        self.range_pixFromCenterErr = np.arange(-160, 160, 1)
-        self.range_servoAngleChange = np.arange(-10, 10, 1)
+        self.calcInputMemFuncActivations(-160)
+
+    def createhorizontalMembershipFunctions(self, plotOrNot):
+        # Generate input and output analog variable ranges
+        self.range_pixFromCenterErrHorz = np.arange(-160, 160, 1)
+        self.range_servoAngleChangeHorz = np.arange(-10, 10, 1)
 
         # Generate fuzzy membership functions for Inputs
-        self.pixFromCenterErr_VeryLeft = fuzz.trimf(
-            self.range_pixFromCenterErr, [-240, -160, -96.1])
-        self.pixFromCenterErr_Left = fuzz.trimf(
-            self.range_pixFromCenterErr, [-160, -80, 0])
-        self.pixFromCenterErr_Center = fuzz.trimf(
-            self.range_pixFromCenterErr, [-80, 0, 80])
-        self.pixFromCenterErr_Right = fuzz.trimf(
-            self.range_pixFromCenterErr, [0, 80, 160])
-        self.pixFromCenterErr_VeryRight = fuzz.trimf(
-            self.range_pixFromCenterErr, [80, 160, 240])
+        self.mf_pixFromCenterErrHorz_VeryLeft = fuzz.trimf(
+            self.range_pixFromCenterErrHorz, [-240, -160, -96.1])
+        self.mf_pixFromCenterErrHorz_Left = fuzz.trimf(
+            self.range_pixFromCenterErrHorz, [-160, -80, 0])
+        self.mf_pixFromCenterErrHorz_Center = fuzz.trimf(
+            self.range_pixFromCenterErrHorz, [-80, 0, 80])
+        self.mf_pixFromCenterErrHorz_Right = fuzz.trimf(
+            self.range_pixFromCenterErrHorz, [0, 80, 160])
+        self.mf_pixFromCenterErrHorz_VeryRight = fuzz.trimf(
+            self.range_pixFromCenterErrHorz, [80, 160, 240])
 
         # Generate fuzzy membership functions for Output
-        self.servoAngleChange_VeryLeft = fuzz.trimf(
-            self.range_servoAngleChange, [-15, -10, -5])
-        self.servoAngleChange_Left = fuzz.trimf(
-            self.range_servoAngleChange, [-10, -5, 0])
-        self.servoAngleChange_DontMove = fuzz.trimf(
-            self.range_servoAngleChange, [-5, 0, 5])
-        self.servoAngleChange_Right = fuzz.trimf(
-            self.range_servoAngleChange, [0, 5, 10])
-        self.servoAngleChange_VeryRight = fuzz.trimf(
-            self.range_servoAngleChange, [5, 10, 15])
+        self.mf_servoAngleChangeHorz_VeryLeft = fuzz.trimf(
+            self.range_servoAngleChangeHorz, [-15, -10, -5])
+        self.mf_servoAngleChangeHorz_Left = fuzz.trimf(
+            self.range_servoAngleChangeHorz, [-10, -5, 0])
+        self.mf_servoAngleChangeHorz_DontMove = fuzz.trimf(
+            self.range_servoAngleChangeHorz, [-5, 0, 5])
+        self.mf_servoAngleChangeHorz_Right = fuzz.trimf(
+            self.range_servoAngleChangeHorz, [0, 5, 10])
+        self.mf_servoAngleChangeHorz_VeryRight = fuzz.trimf(
+            self.range_servoAngleChangeHorz, [5, 10, 15])
 
         if(plotOrNot):
             # Visualize these universes and membership functions
             fig, (ax0, ax1) = plt.subplots(nrows=2)  # , figsize=(8, 9))
 
-            ax0.plot(self.range_pixFromCenterErr, self.pixFromCenterErr_VeryLeft,
+            ax0.plot(self.range_pixFromCenterErrHorz, self.mf_pixFromCenterErrHorz_VeryLeft,
                      color='b', linewidth=1.5, label='Very Left')
-            ax0.plot(self.range_pixFromCenterErr, self.pixFromCenterErr_Left,
+            ax0.plot(self.range_pixFromCenterErrHorz, self.mf_pixFromCenterErrHorz_Left,
                      'c', linewidth=1.5, label='Left')
-            ax0.plot(self.range_pixFromCenterErr, self.pixFromCenterErr_Center,
+            ax0.plot(self.range_pixFromCenterErrHorz, self.mf_pixFromCenterErrHorz_Center,
                      'g', linewidth=1.5, label='Center')
-            ax0.plot(self.range_pixFromCenterErr, self.pixFromCenterErr_Right,
+            ax0.plot(self.range_pixFromCenterErrHorz, self.mf_pixFromCenterErrHorz_Right,
                      'y', linewidth=1.5, label='Right')
-            ax0.plot(self.range_pixFromCenterErr, self.pixFromCenterErr_VeryRight,
+            ax0.plot(self.range_pixFromCenterErrHorz, self.mf_pixFromCenterErrHorz_VeryRight,
                      'r', linewidth=1.5, label='Very Right')
             ax0.set_title(
                 'Input - Pixels From Center Error Horizontal Membership Functions')
             ax0.legend()
 
-            ax1.plot(self.range_servoAngleChange, self.servoAngleChange_VeryLeft,
+            ax1.plot(self.range_servoAngleChangeHorz, self.mf_servoAngleChangeHorz_VeryLeft,
                      'b', linewidth=1.5, label='Very Left')
-            ax1.plot(self.range_servoAngleChange, self.servoAngleChange_Left,
+            ax1.plot(self.range_servoAngleChangeHorz, self.mf_servoAngleChangeHorz_Left,
                      'c', linewidth=1.5, label='Left')
-            ax1.plot(self.range_servoAngleChange, self.servoAngleChange_DontMove,
+            ax1.plot(self.range_servoAngleChangeHorz, self.mf_servoAngleChangeHorz_DontMove,
                      'g', linewidth=1.5, label='Do not move')
-            ax1.plot(self.range_servoAngleChange, self.servoAngleChange_Right,
+            ax1.plot(self.range_servoAngleChangeHorz, self.mf_servoAngleChangeHorz_Right,
                      'y', linewidth=1.5, label='Right')
-            ax1.plot(self.range_servoAngleChange, self.servoAngleChange_VeryRight,
+            ax1.plot(self.range_servoAngleChangeHorz, self.mf_servoAngleChangeHorz_VeryRight,
                      'r', linewidth=1.5, label='Very Right')
             ax1.set_title(
                 'Output - Degrees to Move Horizontal Membership Functions')
@@ -97,3 +97,9 @@ class fuzzyServoSetPointChangeCalc:
             plt.tight_layout()
             plt.draw()
             plt.show()
+
+    def calcInputMemFuncActivations(self, pixFromCenterErrHorz):
+        self.act_pixFromCenterErrHorz_VeryLeft = fuzz.interp_membership(
+            self.range_pixFromCenterErrHorz, self.mf_pixFromCenterErrHorz_VeryLeft, pixFromCenterErrHorz)
+        self.act_pixFromCenterErrHorz_Left = fuzz.interp_membership(
+            self.range_pixFromCenterErrHorz, self.mf_pixFromCenterErrHorz_Left, pixFromCenterErrHorz)
